fb9a02223cf4a3f57ff917bc93488db0e6b8e4c4
hajjanggun <hajjanggun@gmail.com>
2026-02-23 12:52:33 +0900

    feat: 페이지 이동 시 스크롤 최상단 고정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
+import ScrollToTop from '@/components/ScrollToTop';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -66,7 +67,10 @@ export default function RootLayout({
           gtag('config', 'G-HNNGML081Q');
         `}
       </Script>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ScrollToTop />
+        {children}
+      </body>
     </html>
   );
 }
